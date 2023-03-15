e9bca68496e4356b9c957721b8b8c1c79bd6aece
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-03-15 09:19:59 -0700

    push implemented on stack

---
+++ Stacks&Queues/stack.js
@@ -0,0 +1,32 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    push(val){
+        let newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+}
+
+let stack = new Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+console.log(stack)
