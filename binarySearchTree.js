925206be88795faf8d07f1d47f1e48dfc3ac6d3e
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-03-16 09:20:58 -0700

    insert added to BST

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,13 +1,50 @@
-class Node{
-    constructor(value){
-        this.value = value; 
+class Node {
+    constructor(value) {
+        this.value = value;
         this.left = null;
         this.right = null;
     }
 }
 
-class BinarySearchTree{
-    constructor(){
+class BinarySearchTree {
+    constructor() {
         this.root = null;
     }
+    insert(value) {
+        let newNode = new Node(value);
+
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while (true) {
+                if(value === current.value) return undefined;
+                if (value < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (value > current.value) {
+                    if(current.right === null){
+                        current.right = newNode;
+                        return this;
+                    } else{
+                        current = current.right
+                    }
+                }
+            }
+        }
+    }
 }
+
+let tree = new BinarySearchTree();
+tree.insert(100)
+tree.insert(30)
+tree.insert(11)
+tree.insert(30)
+tree.insert(80)
+tree.insert(22)
+console.log(tree)
