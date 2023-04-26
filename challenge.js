efed42f7f9072362daab6ea8d88fff885946aa1d
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-04-25 21:47:49 -0700

    optimized/ advanced fizzbuzz

--- challenge.js
+++ challenge.js
@@ -12,8 +12,27 @@ function fizzBuzz(num){
     return result; 
 };
 
-let test = fizzBuzz(10)
-console.log(test)
+// let test = fizzBuzz(10)
+// console.log(test)
+
+const hardFizzBuzz = (num) => {
+    let result = [];
+    for(let i = 0; i <=  num; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            result.push("FizzBuzz");
+        } else if ( i % 3 === 0){
+            result.push("Fizz");
+        } else if( i % 5 === 0){
+            result.push("Buzz")
+        } else {
+            result.push(i.toString());
+        }
+    }
+    return result
+};
+
+let testTwo = hardFizzBuzz(25)
+console.log(testTwo)
 
 // const fizzBuzz = () => {
 //     console.log("hello")
